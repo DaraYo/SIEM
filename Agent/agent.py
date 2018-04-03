7575dae1212aaac7f87b294001684570e00cee69
AlexSensei <antishadow15@gmail.com>
2018-04-03 16:10:16 +0200

    Doradjen agent

--- Agent/agent.py
+++ Agent/agent.py
@@ -1,11 +1,12 @@
 import glob
 import requests
+import re
 def main():
 	centralAgent = "http://localhost:8000/api/log"
 	f = open("config.config", "r")
 	logs_directorium_list = f.readlines()
 	f.close()
-
+	filter=""
 	messages_dict={}
     
 	for ldl in logs_directorium_list:
@@ -18,39 +19,40 @@ def main():
 			messages_dict[fname[len(fname)-1]]=len(log_messages)
 			print(messages_dict)
 			for message in log_messages:
-				parts = message.split(" ")
-				PRIVERS = parts[0].split(">")
-				PRI = PRIVERS[0].split("<")[1]
-				VERSION=PRIVERS[1]
-				FACILITY=eval(PRI)//20
-				SEVERITY=eval(PRI)%20
-				timestamp = parts[1]
-				if timestamp!='NILVALUE':
-					if timestamp[-1]=='Z':
-						timestamp = timestamp[:-1] + '+0000'
+				if(re.match(filter,message)):
+					parts = message.split(" ")
+					PRIVERS = parts[0].split(">")
+					PRI = PRIVERS[0].split("<")[1]
+					VERSION=PRIVERS[1]
+					FACILITY=eval(PRI)//20
+					SEVERITY=eval(PRI)%20
+					timestamp = parts[1]
+					if timestamp!='NILVALUE':
+						if timestamp[-1]=='Z':
+							timestamp = timestamp[:-1] + '+0000'
+						else:
+							timestamp = timestamp[:-6] + timestamp[-6:].replace(':','')
+					HOSTNAME = parts[2]
+					APPNAME = parts[3]
+					PROCID = parts[4]
+					MSGID= parts[5]
+					SDATAMSG=""
+					for i in range(6,len(parts)):
+						SDATAMSG=SDATAMSG+" "+parts[i]
+					SDATAMSG=SDATAMSG.strip()
+					if(SDATAMSG[0]=='['):
+						SDATA=SDATAMSG.split("]")[0]+"]"
+						MSG=SDATAMSG.split("]")[1]
 					else:
-						timestamp = timestamp[:-6] + timestamp[-6:].replace(':','')
-				HOSTNAME = parts[2]
-				APPNAME = parts[3]
-				PROCID = parts[4]
-				MSGID= parts[5]
-				SDATAMSG=""
-				for i in range(6,len(parts)):
-					SDATAMSG=SDATAMSG+" "+parts[i]
-				SDATAMSG=SDATAMSG.strip()
-				if(SDATAMSG[0]=='['):
-					SDATA=SDATAMSG.split("]")[0]+"]"
-					MSG=SDATAMSG.split("]")[1]
-				else:
-					DATA=SDATAMSG.split(" ")
-				SDATA=DATA[0]
-				MSG=""
-				for i in range(1,len(DATA)):
-					MSG=MSG+" "+DATA[i]
-				MSG=MSG.strip()
-				payload={'facility':FACILITY,'severity':SEVERITY,'version':VERSION,'timestamp':timestamp,'hostname':HOSTNAME,'appname':APPNAME,'procid':PROCID,'msgid':MSGID,'structuredData':SDATA,'msg':MSG}
-				print("Sent to server: "+payload['msg'])
-				requests.post(centralAgent,data=payload)
+						DATA=SDATAMSG.split(" ")
+					SDATA=DATA[0]
+					MSG=""
+					for i in range(1,len(DATA)):
+						MSG=MSG+" "+DATA[i]
+					MSG=MSG.strip()
+					payload={'facility':FACILITY,'severity':SEVERITY,'version':VERSION,'timestamp':timestamp,'hostname':HOSTNAME,'appname':APPNAME,'procid':PROCID,'msgid':MSGID,'structuredData':SDATA,'msg':MSG}
+					print("Sent to server: "+payload['msg'])
+					requests.post(centralAgent,data=payload)
 
 				
 	while True:
@@ -63,39 +65,40 @@ def main():
 				fname=log.split('\\')	
 				if(messages_dict[fname[len(fname)-1]]<len(log_messages)):
 					for i in range(messages_dict[fname[len(fname)-1]],len(log_messages)):
-						parts = log_messages[i].split(" ")
-						PRIVERS = parts[0].split(">")
-						PRI = PRIVERS[0].split("<")[1]
-						VERSION=PRIVERS[1]
-						FACILITY=eval(PRI)//20
-						SEVERITY=eval(PRI)%20
-						timestamp = parts[1]
-						if(timestamp!='NILVALUE'):
-							if timestamp[-1]=='Z':
-								timestamp = timestamp[:-1] + '+0000'
+						if(re.match(filter,message)):
+							parts = log_messages[i].split(" ")
+							PRIVERS = parts[0].split(">")
+							PRI = PRIVERS[0].split("<")[1]
+							VERSION=PRIVERS[1]
+							FACILITY=eval(PRI)//20
+							SEVERITY=eval(PRI)%20
+							timestamp = parts[1]
+							if(timestamp!='NILVALUE'):
+								if timestamp[-1]=='Z':
+									timestamp = timestamp[:-1] + '+0000'
+								else:
+									timestamp = timestamp[:-6] + timestamp[-6:].replace(':','')
+							HOSTNAME = parts[2]
+							APPNAME = parts[3]
+							PROCID = parts[4]
+							MSGID= parts[5]
+							SDATAMSG=""
+							for i in range(6,len(parts)):
+								SDATAMSG=SDATAMSG+" "+parts[i]
+							SDATAMSG=SDATAMSG.strip()
+							if(SDATAMSG[0]=='['):
+								SDATA=SDATAMSG.split("]")[0]+"]"
+								MSG=SDATAMSG.split("]")[1]
 							else:
-								timestamp = timestamp[:-6] + timestamp[-6:].replace(':','')
-						HOSTNAME = parts[2]
-						APPNAME = parts[3]
-						PROCID = parts[4]
-						MSGID= parts[5]
-						SDATAMSG=""
-						for i in range(6,len(parts)):
-							SDATAMSG=SDATAMSG+" "+parts[i]
-						SDATAMSG=SDATAMSG.strip()
-						if(SDATAMSG[0]=='['):
-							SDATA=SDATAMSG.split("]")[0]+"]"
-							MSG=SDATAMSG.split("]")[1]
-						else:
-							DATA=SDATAMSG.split(" ")
-						SDATA=DATA[0]
-						MSG=""
-						for i in range(1,len(DATA)):
-							MSG=MSG+" "+DATA[i]
-						MSG=MSG.strip()
-						payload={'facility':FACILITY,'severity':SEVERITY,'version':VERSION,'timestamp':timestamp,'hostname':HOSTNAME,'appname':APPNAME,'procid':PROCID,'msgid':MSGID,'structuredData':SDATA,'msg':MSG}
-						print("New message sent to server: "+payload['msg'])
-						requests.post(centralAgent,data=payload)
+								DATA=SDATAMSG.split(" ")
+							SDATA=DATA[0]
+							MSG=""
+							for i in range(1,len(DATA)):
+								MSG=MSG+" "+DATA[i]
+							MSG=MSG.strip()
+							payload={'facility':FACILITY,'severity':SEVERITY,'version':VERSION,'timestamp':timestamp,'hostname':HOSTNAME,'appname':APPNAME,'procid':PROCID,'msgid':MSGID,'structuredData':SDATA,'msg':MSG}
+							print("New message sent to server: "+payload['msg'])
+							requests.post(centralAgent,data=payload)
 					messages_dict[fname[len(fname)-1]]=len(log_messages)
     
 if __name__=="__main__":
