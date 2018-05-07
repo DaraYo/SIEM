a35f365b606165af4f36ccc754d37d8e1c087545
AlexSensei <antishadow15@gmail.com>
2018-05-08 00:34:26 +0200

    Doradjen prikaz alarma i kreiranje pravila za okidanje alarma

--- Server/siemServer/alarmService/urls.py
+++ Server/siemServer/alarmService/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
     path('allAlarms', views.getAlarmLogs),
+	path('alarmRules', views.getAlarmRules,name='alarmRules'),
+	path('createRule',views.createRule,name='createRule'),
+	url(r'^generateAlarm', views.generateAlarm, name = "generateAlarm"),
 ]
